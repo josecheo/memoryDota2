a33e184920c8b6977dc8c6d4e3cea5826c2be2c3
Jose Alvarez <joseche0.ja@gmail.com>
2019-12-21 18:59:51 -0500

    Soy yo
    el mejor

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 function generador(){
-
-for(let i=0; i<20; i++){
-
-   var x = Math.floor((Math.random()*20)+1);
-   console.log(x);
+document.getElementById("divImagenes").innerHTML="";
+matriz=[];
+while(matriz.length<20){
+var nrandom = Math.floor((Math.random()*20)+1);
+if(matriz.includes(nrandom)) continue;
+matriz.push(nrandom);
     document.getElementById("divImagenes").innerHTML+=`
-   <img src="img/${x}.png";" >
+   <img id="img" class="tapa" src="img/${nrandom}.png" onclick="destapar()">
    `;
+ }
+console.log(matriz);
 }
+  
+function destapar(){
+
+console.log("aqui destapa");
+
 }
+
+
+
+
+
+
 
